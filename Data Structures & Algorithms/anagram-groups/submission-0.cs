27238e54d43ecd75103f6df87427b6f21912447b
Rémi <rem_ay@hotmail.fr>
2026-07-06 11:23:27 +1200

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        var dict = new Dictionary<string, List<string>>();
+
+        foreach (var word in strs)
+        {
+            var freq = new int[26];
+            foreach (var c in word)
+            {
+                freq[c - 'a'] += 1;
+            }
+            var key = string.Join(",", freq);
+            if (!dict.TryGetValue(key, out List<string>? value))
+            {
+                value = [];
+                dict[key] = value;
+            }
+
+            value.Add(word);
+        }
+
+        var result = new List<List<string>>();
+        foreach (var kvp in dict)
+        {
+            result.Add(kvp.Value);
+        }
+
+        return result;
+    }
+}
